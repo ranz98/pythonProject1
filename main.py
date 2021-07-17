f61f631506ce0ea1edee88f84edb9859460ad7ce
ranz98 <ranishkap@gmail.com>
2021-07-17 13:59:21 +0530

    array test

---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+
+import sys
+
+#xyz = sys.argv[1]
+
+D = {"a":"1","b":"2","c":"3"}
+
+s = json.dumps(D)
+
+print(s)
+
+#world='world'
+#rint (xyz)
+#print('boy1')
+#p#rint('boy2')
+#print('boy3')
